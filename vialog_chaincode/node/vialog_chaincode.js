a1a3cd3eb2f579b40011ca41770904b59e61fc43
Tamás Turcsányi <tamas@vialog.app>
2020-06-24 19:00:51 +0200

    Initial commit

---
+++ vialog_chaincode/node/vialog_chaincode.js
@@ -0,0 +1,86 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+const shim = require('fabric-shim');
+const util = require('util');
+
+
+var Chaincode = class {
+
+  // Initialize the chaincode
+  async Init(stub) {
+    let ret = stub.getFunctionAndParameters();
+    console.info(ret);
+    console.info('=========== Vialog Chaincode ===========');
+    return shim.success();
+  }
+
+  async Invoke(stub) {
+    let ret = stub.getFunctionAndParameters();
+    console.info(ret);
+    let method = this[ret.fcn];
+    if (!method) {
+      console.log('no method of name:' + ret.fcn + ' found');
+      return shim.success();
+    }
+    try {
+      let payload = await method(stub, ret.params);
+      return shim.success(payload);
+    } catch (err) {
+      console.log(err);
+      return shim.error(err);
+    }
+  }
+
+  async addVideo(stub, args) {
+    if (args.length != 3) {
+      throw new Error('Incorrect number of arguments. Expecting 3');
+    }
+
+    let id = args[0];
+    let created = args[1];
+    let state = args[2];
+
+    let video = {
+      Id: id,
+      Created: created,
+      State: state
+    };
+
+    let buffer = Buffer.from(JSON.stringify(video));
+    await stub.putState(id, buffer);
+
+    return buffer;
+  }
+
+  // query callback representing the query of a chaincode
+  async query(stub, args) {
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting id of the video to query')
+    }
+
+    let jsonResp = {};
+    let id = args[0];
+
+    // Get the state from the ledger
+    let Avalbytes = await stub.getState(id);
+    if (!Avalbytes) {
+      jsonResp.error = 'Failed to get state for ' + id;
+      throw new Error(JSON.stringify(jsonResp));
+    }
+
+    let video = JSON.parse(Avalbytes.toString());
+
+    
+
+
+    console.info('Query Response:');
+    console.info(video);
+    return video;
+  }
+};
+
+shim.start(new Chaincode());
