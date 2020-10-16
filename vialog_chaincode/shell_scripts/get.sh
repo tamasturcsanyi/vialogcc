6d9f0d5abe06cbdee455aa230485ab509abdacd3
Tamás Turcsányi <tamas@vialog.app>
2020-10-16 19:52:31 +0200

    Updated scripts

--- vialog_chaincode/shell_scripts/get.sh
+++ vialog_chaincode/shell_scripts/get.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -x #echo on
 
-export CORE_PEER_ADDRESS="peer2:7051"
-export CORE_PEER_MSPCONFIGPATH="/root/admin/msp"
-export CORE_PEER_TLS_ROOTCERT_FILE="/root/${PEER2_HOST}/tls-msp/tlscacerts/tls-${TLSCA_HOST}-7054.pem"
-export CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS
-export CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH
-export CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE
+CHAINCODE_NAME="vialog_dev_cc"
+CHANNEL_NAME="appchannel"
+QUERY_PARAMS='{"Args":["query","1"]}'
 
-peer chaincode query -C appchannel -n vialog_dev_cc -c '{"Args":["query","1"]}'
+export PEER_HOST=peer2
+export CORE_PEER_ADDRESS=${PEER_HOST}:7051
+export CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
+
+CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer chaincode query -C $CHANNEL_NAME -n $CHAINCODE_NAME -c $QUERY_PARAMS
--- vialog_chaincode/shell_scripts/install.sh
+++ vialog_chaincode/shell_scripts/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -x #echo on
 
-export CORE_PEER_ADDRESS="peer2:7051"
-export CORE_PEER_MSPCONFIGPATH="/root/admin/msp"
-export CORE_PEER_TLS_ROOTCERT_FILE="/root/${PEER2_HOST}/tls-msp/tlscacerts/tls-${TLSCA_HOST}-7054.pem"
-export CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS
-export CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH
-export CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE
+CHAINCODE_NAME="vialog_dev_cc"
+CHAINCODE_VERSION="1.0"
+CHAINCODE_SRC_CODE_PATH="/root/CLI/vialogcc/vialog_chaincode/node"
 
-peer chaincode install -n vialog_dev_cc -v 1.0 -l node -p /root/CLI/vialogcc/vialog_chaincode/node
+export PEER_HOST=peer2
+export CORE_PEER_ADDRESS=${PEER_HOST}:7051
+export CORE_PEER_MSPCONFIGPATH=/root/CLI/${ORGCA_HOST}/${ADMIN_USER}/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/root/CLI/${ORGCA_HOST}/${PEER_HOST}/msp/tls/ca.crt
+
+CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l node -p $CHAINCODE_SRC_CODE_PATH
+
