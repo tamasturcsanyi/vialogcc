cedabed512d7db270c950f083efcd6ba1f4a6eb1
Tamás Turcsányi <tamas@vialog.app>
2020-10-16 20:00:24 +0200

    Updated put script

--- vialog_chaincode/shell_scripts/put.sh
+++ vialog_chaincode/shell_scripts/put.sh
@@ -9,4 +9,4 @@ export CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH
 export CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE
 
 
-peer chaincode invoke -C appchannel -n vialog_dev_cc -c '{"Args":["addVideo","2222","2020-06-24T18:10:08","Added"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
+peer chaincode invoke -C appchannel -n vialog_dev_cc -c '{"Args":["addVideoEvent", "1", "testVideoToken", "", "2020-06-24T18:10:08", "6.14", "240x400", "", "890", "1", "0", "ef7f9b47-8f8f-4a59-b48a-f97e7571a7d6", "2020-06-24T18:10:08", "", "0", "Created", "Normal"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
